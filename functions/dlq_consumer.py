362d2a8b3f9ef0a248c8af458a7e77a1a3aec6b0
Dillon <dpeterson@loc.gov>
2023-07-17 14:23:38 -0500

    Add messages to DynamoDB table by DLQ handler upon failure
    Former-commit-id: bd066fd67e48ceeea1281a6534803ff6b34f16c4

--- functions/dlq_consumer.py
+++ functions/dlq_consumer.py
@@ -2,47 +2,50 @@ import boto3
 import logging
 import os
 import json
-dynamodb = boto3.resource('dynamodb')
-queue_url = os.environ.get('QUEUE_URL')
+
+dynamodb = boto3.resource("dynamodb")
+queue_url = os.environ.get("QUEUE_URL")
+
 
 def handle_failed_message(message, table, job_id):
     # Increment the count of failed messages in the job summary
     table.update_item(
-        Key={
-            'pk': 'JOB',
-            'sk': job_id
-        },
-        UpdateExpression='ADD #FailureCount :inc',
-        ExpressionAttributeNames={
-            '#FailureCount': 'FailureCount'
-        },
-        ExpressionAttributeValues={
-            ':inc': 1
-        }
+        Key={"pk": "JOB", "sk": job_id},
+        UpdateExpression="ADD #FailureCount :inc",
+        ExpressionAttributeNames={"#FailureCount": "FailureCount"},
+        ExpressionAttributeValues={":inc": 1},
     )
 
+
     table.update_item(
-        Key={
-            'pk': 'JOB',
-            'sk': job_id
-        },
-        UpdateExpression='SET #failed_messages = list_append(if_not_exists(#failed_messages, :empty_list), :message)',
-        ExpressionAttributeNames={
-            '#failed_messages': 'failed_messages'
-        },
+        Key={"pk": "JOB", "sk": job_id},
+        UpdateExpression="SET #failed_messages = list_append(if_not_exists(#failed_messages, :empty_list), :message)",
+        ExpressionAttributeNames={"#failed_messages": "failed_messages"},
         ExpressionAttributeValues={
-            ':message': message,
-            ':empty_list': []
-        }
+            ":message": [message],  # Wrap the message in a list
+            ":empty_list": [],
+        },
     )
+
 
 def handler(event, context):
     logging.info("Number of failed messages: {}".format(len(event["Records"])))
+    table = dynamodb.Table(os.getenv("TABLE_NAME"))
+
     for record in event["Records"]:
         message = json.loads(record["body"])
-        print(message)
         job_id = message["JobId"]
+        resp = table.update_item(
+            Key={"pk": "JOB", "sk": job_id},
+            UpdateExpression="SET RemainingMessages = RemainingMessages - :dec",
+            ExpressionAttributeValues={
+                ":dec": len(event["Records"]),
+            },
+            ReturnValues="UPDATED_NEW",
+        )
+        print(message)
         table = dynamodb.Table(os.getenv("TABLE_NAME"))
+
         try:
             # Log the failed message
             # logging.error(f"Processing of message {message['MessageId']} failed.")
