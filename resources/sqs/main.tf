e1192c6e0be47fa854bd194cddb195ff3065f8d9
Dillon <dpeterson@loc.gov>
2023-10-06 14:55:54 -0500

    Remove extraneous SQS queue
    Former-commit-id: 98d140041405f65150481ff9a53abc0e73533eee [formerly 287fdb3de2978f59b5d27aab1e05f51f7c877409]
    Former-commit-id: cae15175a79ceb06734096f6e954d6e93dd96e17 [formerly 323710edf2d2e9ec743513ddf6a1aa28934a31e3]
    Former-commit-id: ae9dffca4309f478b58eb07033ff73beee021398
    Former-commit-id: c9a22cfb6bba2c4150e2f5a87d0e68b188517449

--- resources/sqs/main.tf
+++ resources/sqs/main.tf
@@ -12,17 +12,17 @@ resource "aws_sqs_queue" "pdf_queue" {
 }
 
 # Queue to serve as source messages for Lambda OCR Reprocessing ALTO Consumer
-resource "aws_sqs_queue" "alto_queue" {
-  name                       = "ndnp-open-ocr-alto-consumer-sqs-queue"
-  delay_seconds              = 0
-  max_message_size           = 1024
-  message_retention_seconds  = 345600
-  visibility_timeout_seconds = 900
-  redrive_policy = jsonencode({
-    deadLetterTargetArn = aws_sqs_queue.alto_dlq.arn
-    maxReceiveCount     = 1
-  })
-}
+# resource "aws_sqs_queue" "alto_queue" {
+#   name                       = "ndnp-open-ocr-alto-consumer-sqs-queue"
+#   delay_seconds              = 0
+#   max_message_size           = 1024
+#   message_retention_seconds  = 345600
+#   visibility_timeout_seconds = 900
+#   redrive_policy = jsonencode({
+#     deadLetterTargetArn = aws_sqs_queue.alto_dlq.arn
+#     maxReceiveCount     = 1
+#   })
+# }
 
 # Dead letter queue to catch failed PDF jobs
 resource "aws_sqs_queue" "pdf_dlq" {
@@ -31,15 +31,15 @@ resource "aws_sqs_queue" "pdf_dlq" {
 }
 
 # Dead letter queue to catch failed ALTO jobs
-resource "aws_sqs_queue" "alto_dlq" {
-  name = "${var.queue_name}_alto_dlq"
-  visibility_timeout_seconds = 60
-}
+# resource "aws_sqs_queue" "alto_dlq" {
+#   name = "${var.queue_name}_alto_dlq"
+#   visibility_timeout_seconds = 60
+# }
 
 # IAM policy to grant access to SQSyes
-data "aws_iam_policy_document" "sqs" {
-  statement {
-    actions   = ["sqs:*"]
-    resources = [aws_sqs_queue.pdf_dlq.arn, aws_sqs_queue.alto_dlq.arn]
-  }
-}
+# data "aws_iam_policy_document" "sqs" {
+#   statement {
+#     actions   = ["sqs:*"]
+#     resources = [aws_sqs_queue.pdf_dlq.arn, aws_sqs_queue.alto_dlq.arn]
+#   }
+# }
