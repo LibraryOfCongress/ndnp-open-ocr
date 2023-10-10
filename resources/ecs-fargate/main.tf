c3f897342e4391bf966b28c44ac5e397936639fc
Dillon <dpeterson@loc.gov>
2023-10-10 16:01:11 -0500

    Add vpc and security group and changed bucket name to make work with LOC account
    Former-commit-id: 2ac45f22d2d97306f43cf1f20fbb926076075ddf [formerly 8352abd6e86ea35ab6272e5996a05977c54e50df]
    Former-commit-id: d79a22da9a0cce22e0ab41f7c5b2362d55927982 [formerly 0ea0d6fd8f6f88f982164f8e46346666837e1508]
    Former-commit-id: 4aaf94430c4dc7392c2cd796d83eaded9fb6823f
    Former-commit-id: 81828326cce69c4572e71bbf0961b39fc24d3ab7

--- resources/ecs-fargate/main.tf
+++ resources/ecs-fargate/main.tf
@@ -1,49 +1,93 @@
 resource "aws_ecr_repository" "repo" {
   name = "ndnp-open-ocr-container-repo"
+
+  tags = {
+    Name = "ndnp-open-ocr"
+  }
 }
 
 resource "aws_ecs_cluster" "cluster" {
   name = "ndnp-open-ocr-fargate-cluster"
+
+  tags = {
+    Name = "ndnp-open-ocr"
+  }
 }
 
-resource "aws_ecs_task_definition" "task_def" {
-  family                   = var.task_family
-  network_mode             = "awsvpc"
-  requires_compatibilities = ["FARGATE"]
-  cpu                      = 256
-  memory                   = 512
-  execution_role_arn       = var.execution_role_arn
-  task_role_arn            = var.task_role_arn
+# resource "aws_ecs_task_definition" "task_def" {
+#   family                   = var.task_family
+#   network_mode             = "awsvpc"
+#   requires_compatibilities = ["FARGATE"]
+#   cpu                      = 256
+#   memory                   = 512
+#   execution_role_arn       = var.execution_role_arn
+#   task_role_arn            = var.task_role_arn
 
-  container_definitions = jsonencode([{
-    name  = var.container_name
-    image = var.container_image
-    portMappings = [{
-      containerPort = var.container_port
-      hostPort      = var.container_port
-    }]
-    environment = [
-      {
-        name  = "SQS_QUEUE_URL",
-        value = var.sqs_queue_url
-      },
-      {
-        name  = "TABLE_NAME",
-        value = "AnotherValue"
-      }
-    ]
-  }])
+#   container_definitions = jsonencode([{
+#     name  = var.container_name
+#     image = var.container_image
+#     portMappings = [{
+#       containerPort = var.container_port
+#       hostPort      = var.container_port
+#     }]
+#     environment = [
+#       {
+#         name  = "SQS_QUEUE_URL",
+#         value = var.sqs_queue_url
+#       },
+#       {
+#         name  = "TABLE_NAME",
+#         value = "AnotherValue"
+#       }
+#     ]
+#   }])
+
+#   tags = {
+#     Name = "ndnp-open-ocr"
+#   }
+# }
+
+# resource "aws_ecs_service" "service" {
+#   name            = var.service_name
+#   cluster         = aws_ecs_cluster.cluster.id
+#   task_definition = aws_ecs_task_definition.task_def.arn
+#   launch_type     = "FARGATE"
+#   desired_count   = var.desired_count
+
+#   network_configuration {
+#     subnets         = var.subnets
+#     security_groups = var.security_groups
+#   }
+
+#   tags = {
+#     Name = "ndnp-open-ocr"
+#   }
+# }
+
+resource "aws_vpc" "vpc" {
+  cidr_block = "10.0.0.0/16"
+
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+
+  tags = {
+    Name = "ndnp-open-ocr"
+  }
 }
 
-resource "aws_ecs_service" "service" {
-  name            = var.service_name
-  cluster         = aws_ecs_cluster.cluster.id
-  task_definition = aws_ecs_task_definition.task_def.arn
-  launch_type     = "FARGATE"
-  desired_count   = var.desired_count
+resource "aws_security_group" "ecs_tasks" {
+  vpc_id = aws_vpc.vpc.id
 
-  network_configuration {
-    subnets         = var.subnets
-    security_groups = var.security_groups
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # Add more ingress/egress rules as needed.
+
+  tags = {
+    Name = "ndnp-open-ocr"
   }
 }
