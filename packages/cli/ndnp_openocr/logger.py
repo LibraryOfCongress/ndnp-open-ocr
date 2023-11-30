9b42bf714bf0dc86aa0600f022387f42d636f086
Dillon <dpeterson@loc.gov>
2023-11-30 13:39:29 -0600

    Remove unneccessary functions no longer being used
    Former-commit-id: 66244a93d544368a7d4d9fe53262d668b748b2b8

--- packages/cli/ndnp_openocr/logger.py
+++ packages/cli/ndnp_openocr/logger.py
@@ -1,7 +1,8 @@
 import logging
 import os
 
-def setup_logging(log_filename='ndnp_open_ocr.log'):
+
+def setup_logging(log_filename="ndnp_open_ocr.log"):
     current_directory = os.getcwd()
     log_file_path = os.path.join(current_directory, log_filename)
 
@@ -11,12 +12,15 @@ def setup_logging(log_filename='ndnp_open_ocr.log'):
 
     file_handler = logging.FileHandler(log_file_path)
 
-    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    formatter = logging.Formatter(
+        "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+    )
     file_handler.setFormatter(formatter)
 
     logger.addHandler(file_handler)
 
     return logger
+
 
 # Configure and export the logger
 logger = setup_logging()
