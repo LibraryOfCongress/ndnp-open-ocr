2f3e0e1acb4d65f1ccfaaf45f3bec2c8de777667
Dillon <dpeterson@loc.gov>
2024-09-29 12:59:12 -0500

    CHRONAM-2509: Terraform attempt deploy to dev
    Former-commit-id: 9041247dd61e59bfaa2b6c9e3ba64cd611038fc0

--- main.tf
+++ main.tf
@@ -1,6 +1,14 @@
 # Main Terraform file to declare NDNP Open OCR resources that have to be created
 # in AWS to run the pipeline.
-
+terraform {
+  backend "http" {
+    address        = "https://git.loc.gov/api/v4/projects/2983/terraform/state/${terraform.workspace}"
+    lock_address   = "https://git.loc.gov/api/v4/projects/2983/terraform/state/${terraform.workspace}/lock"
+    unlock_address = "https://git.loc.gov/api/v4/projects/2983/terraform/state/${terraform.workspace}/lock"
+    username       = var.gitlab_username
+    password       = var.gitlab_token
+  }
+}
 
 # S3 bucket related resources.
 module "s3" {
