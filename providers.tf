a795b3a378b4fbeb4bd8eda60793460e28b2ec6b
Dillon <dpeterson@loc.gov>
2024-09-30 22:22:19 -0500

    CHRONAM-2509: Update provider
    Former-commit-id: 6592efef0abafe8d4ee4775a98326e448feb0d90

--- providers.tf
+++ providers.tf
@@ -1,20 +1,20 @@
 # Module: providers
 
-variable "aws_region" {
-  description = "AWS region"
-  type        = string
-  default     = "us-east-2"
-}
+# variable "aws_region" {
+#   description = "AWS region"
+#   type        = string
+#   default     = "us-east-2"
+# }
 
-variable "aws_profile" {
-  description = "AWS profile"
-  type        = string
-  default     = "loc1"
-}
+# variable "aws_profile" {
+#   description = "AWS profile"
+#   type        = string
+#   default     = "loc1"
+# }
 
-provider "aws" {
-  region = var.aws_region
-}
+# provider "aws" {
+#   region = var.aws_region
+# }
 
 terraform {
   required_providers {
