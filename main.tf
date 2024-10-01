7fc0586215d00d3d8183a011464dcb98f77221cb
Dillon <dpeterson@loc.gov>
2024-09-30 22:42:02 -0500

    CHRONAM-2509: Add remote gitlab backend
    Former-commit-id: c0f030ca31dd0f8188144d84ef81c3fa5cd9e470

--- main.tf
+++ main.tf
@@ -1,14 +1,14 @@
 # Main Terraform file to declare NDNP Open OCR resources that have to be created
 # in AWS to run the pipeline.
-# terraform {
-#   backend "http" {
-#     address        = "https://git.loc.gov/api/v4/projects/2983/terraform/state/${terraform.workspace}"
-#     lock_address   = "https://git.loc.gov/api/v4/projects/2983/terraform/state/${terraform.workspace}/lock"
-#     unlock_address = "https://git.loc.gov/api/v4/projects/2983/terraform/state/${terraform.workspace}/lock"
-#     username       = var.gitlab_username
-#     password       = var.gitlab_token
-#   }
-# }
+terraform {
+  backend "http" {
+    address        = "https://git.loc.gov/api/v4/projects/2983/terraform/state/${terraform.workspace}"
+    lock_address   = "https://git.loc.gov/api/v4/projects/2983/terraform/state/${terraform.workspace}/lock"
+    unlock_address = "https://git.loc.gov/api/v4/projects/2983/terraform/state/${terraform.workspace}/lock"
+    username       = "gitlab-ci-token"
+    password       = "${CI_JOB_TOKEN}"
+  }
+}
 
 # S3 bucket related resources.
 module "s3" {
