b189d3a9f162c4e5153816553ea19f5ba891a5d1
Dillon <dpeterson@loc.gov>
2024-09-30 22:50:40 -0500

    CHRONAM-2509: Add TF backend config to CI script
    Former-commit-id: 257e07955375e05c51efca778c383175433f1411

--- main.tf
+++ main.tf
@@ -1,14 +1,14 @@
 # Main Terraform file to declare NDNP Open OCR resources that have to be created
 # in AWS to run the pipeline.
-terraform {
-  backend "http" {
-    address        = "https://git.loc.gov/api/v4/projects/2983/terraform/state/dev"
-    lock_address   = "https://git.loc.gov/api/v4/projects/2983/terraform/state/dev/lock"
-    unlock_address = "https://git.loc.gov/api/v4/projects/2983/terraform/state/dev/lock"
-    username       = "gitlab-ci-token"
-    password       = "${CI_JOB_TOKEN}"
-  }
-}
+# terraform {
+#   backend "http" {
+#     address        = "https://git.loc.gov/api/v4/projects/2983/terraform/state/dev"
+#     lock_address   = "https://git.loc.gov/api/v4/projects/2983/terraform/state/dev/lock"
+#     unlock_address = "https://git.loc.gov/api/v4/projects/2983/terraform/state/dev/lock"
+#     username       = "gitlab-ci-token"
+#     password       = "${CI_JOB_TOKEN}"
+#   }
+# }
 
 # S3 bucket related resources.
 module "s3" {
