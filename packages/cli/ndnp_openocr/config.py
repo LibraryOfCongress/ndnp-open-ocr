d4deee40f403ca67b8917543386970e25d75bae6
Dillon <dpeterson@loc.gov>
2024-11-12 21:38:14 -0600

    CHRONAM-2533: Move config.py
    Former-commit-id: 7338426945725e38b1fcaaa6bdb6e6348c4c41bc

---
+++ packages/cli/ndnp_openocr/config.py
@@ -0,0 +1,3 @@
+OUTPUT_BUCKET_NAME = "ndnp-open-ocr-output-bucket-development-deployment"
+SCHEDULER_ARN = "arn:aws:lambda:us-east-2:342134162356:function:ndnp-open-ocr-scheduler-lambda-function-development-deployment"
+GET_JOB_ARN = "arn:aws:lambda:us-east-2:342134162356:function:ndnp-open-ocr-get-job-lambda-function-development-deployment"
