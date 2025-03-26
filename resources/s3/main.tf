b7d6c8276b360d3b6e17808372a953f2aa87aa1c
Muhibo Yusuf <myus@loc.gov>
2025-03-26 14:24:04 -0400

    CHRONAM-2632 - Adding ssl requests only policy statement
    Former-commit-id: f53caea796ef4e25a37b6332be857fd2cc51479d

--- resources/s3/main.tf
+++ resources/s3/main.tf
@@ -1,7 +1,26 @@
 data "aws_iam_policy_document" "s3" {
   statement {
     actions   = ["s3:*"]
-    resources = ["arn:aws:s3:::${var.bucket_name}/*"]
+    resources = [
+      "arn:aws:s3:::${var.bucket_name}/*"
+    ]
+  }
+  # Enforce SSL
+  statement {
+    sid    = "AllowSSLRequestsOnly"
+    effect = "Deny"
+    actions   = ["s3:*"]
+    # TODO: Should the environment name be included in the bucket name?
+    resources = [
+      "arn:aws:s3:::${var.bucket_name}",
+      "arn:aws:s3:::${var.bucket_name}/*"
+    ]
+
+    condition {
+      test     = "Bool"
+      variable = "aws:SecureTransport"
+      values   = ["false"]
+    }
   }
 }
 
