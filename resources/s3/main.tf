8593d15157ca7695866796522bbf3edf69fecdee
Muhibo Yusuf <myus@loc.gov>
2025-03-27 13:13:02 -0400

    CHRONAM-2644 - Adding back block_public_access
    Former-commit-id: b80b7d86743f1bd2f8de2ce4f0cd43d30887196f

--- resources/s3/main.tf
+++ resources/s3/main.tf
@@ -39,13 +39,13 @@ data "aws_iam_policy_document" "s3" {
 # }
 
 # Add public access block
-# resource "aws_s3_bucket_public_access_block" "block_public_access" {
-#   bucket = aws_s3_bucket.bucket.id
-#   block_public_acls       = true
-#   ignore_public_acls      = true
-#   block_public_policy     = false
-#   restrict_public_buckets = true
-# }
+resource "aws_s3_bucket_public_access_block" "block_public_access" {
+  bucket = aws_s3_bucket.bucket.id
+  block_public_acls       = true
+  ignore_public_acls      = true
+  block_public_policy     = true
+  restrict_public_buckets = true
+}
 
 resource "aws_s3_bucket" "bucket" {
   bucket = "${var.bucket_name}-${var.env}"
