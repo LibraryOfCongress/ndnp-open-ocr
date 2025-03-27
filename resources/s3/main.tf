73c034ff219acb561c406086067c599e0aef8852
Muhibo Yusuf <myus@loc.gov>
2025-03-27 12:51:12 -0400

    CHRONAM-2644 - Trying block_public_policy to false
    Former-commit-id: 3e231789ca7e74aea07947911e9745cff623a7e2

--- resources/s3/main.tf
+++ resources/s3/main.tf
@@ -43,7 +43,7 @@ resource "aws_s3_bucket_public_access_block" "block_public_access" {
   bucket = aws_s3_bucket.bucket.id
   block_public_acls       = true
   ignore_public_acls      = true
-  block_public_policy     = true
+  block_public_policy     = false
   restrict_public_buckets = true
 }
 
